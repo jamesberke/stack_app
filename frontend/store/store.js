79a67a595bf3287eb31ddfeff7d39f9d018229f1
James Berke <james.berke@gmail.com>
2020-04-11 17:20:41 -0700

    formatting for heroku push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import rootReducer from "../reducers/root_reducer";
 import thunk from "redux-thunk";
 
 const configureStore = (preloadedState = {}) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 export default configureStore;
